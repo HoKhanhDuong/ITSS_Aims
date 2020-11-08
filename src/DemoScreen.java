b86738eefeff01eb81f8399b44033a29edfa6417
HoKhanhDuong <10dragonball.5onepice.1naruto@gmail.com>
2020-11-08 16:32:06 +0700

    test
    test xem

---
+++ src/DemoScreen.java
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import java.awt.Font;
+
+public class DemoScreen extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DemoScreen frame = new DemoScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DemoScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JTextPane txtpnHelloAnhp = new JTextPane();
+		txtpnHelloAnhp.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		txtpnHelloAnhp.setText("Hello anh \u0111\u1EB9p zai");
+		contentPane.add(txtpnHelloAnhp, BorderLayout.CENTER);
+	}
+
+}

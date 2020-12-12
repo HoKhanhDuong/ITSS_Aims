921077b619099e74c10bd08052a2a28b3846daac
Admin <you@example.com>
2020-12-12 22:33:46 +0700

    Update Order History

--- src/user/OrderHisrory.java
+++ src/user/OrderHisrory.java
@@ -22,6 +22,7 @@ import javax.swing.JInternalFrame;
 import javax.swing.JComboBox;
 import java.awt.SystemColor;
 import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
 
 public class OrderHisrory extends MyAccount {
 
@@ -33,6 +34,7 @@ public class OrderHisrory extends MyAccount {
             { "101", "Do Cao Hoc", "7000" } };
     String column[] = { "ID", "NAME", "SALARY" };
     private JTable table;
+    private JTextField textField;
 	public OrderHisrory( Application application) {
 		super(application);
 		JPanel panel = new JPanel();
@@ -47,41 +49,71 @@ public class OrderHisrory extends MyAccount {
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("My Order");
+		JLabel lblNewLabel = new JLabel("1. My Order");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 22));
 		lblNewLabel.setBounds(25, 25, 615, 30);
 		panel_1.add(lblNewLabel);
 		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(10, 65, 650, 336);
+		panel_1.add(panel_2);
+		panel_2.setLayout(null);
+		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setEnabled(false);
-		scrollPane.setViewportBorder(new LineBorder(SystemColor.control));
-		scrollPane.setBounds(10, 69, 650, 451);
-		panel_1.add(scrollPane);
+		scrollPane.setBounds(10, 10, 630, 305);
+		panel_2.add(scrollPane);
 		
 		table = new JTable();
+		table.setEnabled(false);
 		table.setRowHeight(table.getRowHeight() + 20);
-		table.setForeground(new Color(0, 0, 0));
-		table.setCellSelectionEnabled(true);
-		table.setBackground(new Color(255, 255, 255));
-		table.setToolTipText("");
-		table.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{"012345", "11/11/2020", "Hoa cuc dai, Toi thay hoa vang tren co xanh, Shin cau be but chi", "210.000", ""},
-				{"064578", "09/11/2019", "Tuoi tre dang gia bao nhieu", "99.000", "cancel"},
+				{"122334", "Cho toi xin 1 ve di tuoi tho, Co hai con meo ngoi ben cua so", "100.000 d", "OK"},
+				{"", "", null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, "", null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
 			},
 			new String[] {
-				"ID", "Date purchase", "Product", "Total", "Status"
+				"ID ", "Product", "Price", "Status"
 			}
 		));
-		table.getColumnModel().getColumn(1).setResizable(false);
-		table.getColumnModel().getColumn(1).setMinWidth(70);
-		table.getColumnModel().getColumn(2).setPreferredWidth(262);
-		table.getColumnModel().getColumn(3).setPreferredWidth(84);
-		table.getColumnModel().getColumn(4).setPreferredWidth(125);
+		table.getColumnModel().getColumn(1).setPreferredWidth(420);
+		table.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		scrollPane.setViewportView(table);
+		
+		JLabel lblDetailOrder = new JLabel("2. Detail Order");
+		lblDetailOrder.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblDetailOrder.setBounds(25, 404, 615, 30);
+		panel_1.add(lblDetailOrder);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(10, 445, 650, 75);
+		panel_1.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Enter ID Product :");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(10, 10, 140, 30);
+		panel_3.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(157, 10, 230, 30);
+		panel_3.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("View");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(418, 10, 101, 30);
+		panel_3.add(btnNewButton);
+		
+		
 
 	}
 }
